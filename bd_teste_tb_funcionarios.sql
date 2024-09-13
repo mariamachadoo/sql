edc566673c47888bd8547cb446501218c550e927
mariamachadoo <159277809+mariamachadoo@users.noreply.github.com>
2024-09-13 11:52:42 -0300

    p

---
+++ bd_teste_tb_funcionarios.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bd_teste
+-- ------------------------------------------------------
+-- Server version	9.0.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_funcionarios`
+--
+
+DROP TABLE IF EXISTS `tb_funcionarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_funcionarios` (
+  `idfunc` int DEFAULT NULL,
+  `Nome` varchar(50) DEFAULT NULL,
+  `Registro` int DEFAULT NULL,
+  `Guerra` varchar(30) DEFAULT NULL,
+  `Gerencia` varchar(20) DEFAULT NULL,
+  `Det` varchar(20) DEFAULT NULL,
+  `Turno` varchar(20) DEFAULT NULL,
+  `Cargo` varchar(30) DEFAULT NULL,
+  `Atividade` varchar(50) DEFAULT NULL,
+  `Funcao` varchar(30) DEFAULT NULL,
+  `Vinculo` varchar(30) DEFAULT NULL,
+  `Situacao` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_funcionarios`
+--
+
+LOCK TABLES `tb_funcionarios` WRITE;
+/*!40000 ALTER TABLE `tb_funcionarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_funcionarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-13 11:41:43
